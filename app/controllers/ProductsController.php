2bf57a7318d42fe2cf1d5d5ad7075c5e1d315152
Munoz Daniel <munozda87@hotmail.com>
2015-07-09 20:56:48 -0300

    Mostrar busqueda en grilla y paginar.

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -1,5 +1,6 @@
 <?php
-
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
 class ProductsController extends ControllerBase
 {
     public function initialize()
@@ -23,7 +24,44 @@ class ProductsController extends ControllerBase
      */
     public function buscarAction()
     {
-        //...
+        $numeroDePagina =1;
+        if($this->request->isPost())
+        {   //Cuando se accese a traves del post.
+            //Creando las condiciones de la query. Criteria te permite buscar por diferentes criterios, y devuelve una query
+            //de aquellos datos qeu se ingresaron, ignorando los vacios.
+            $query = Criteria::fromInput($this->di,"Products",$this->request->getPost());
+            $this->persistent->searchParams = $query->getParams();
+
+        }
+        else
+        {
+            //Cuando se accede a traves del GET se mueve entre las paginas del paginador.
+            $numberPage = $this->request->getQuery("page", "int");
+        }
+        $parametros = array();
+        if($this->persistent->searchParams)
+        {
+            $parametros = $this->persistent->searchParams;
+        }
+
+        $productos = Products::find($parametros);
+        if(count($productos)==0)
+        {
+            $this->flash->notice("No se encontraron Productos.");
+            return $this->dispatcher->forward(array(
+                'controller' => 'products',
+                'action' => 'index'
+            ));
+        }
+        /*Paginando el resultado que se encuentra en la variable productos.*/
+        $paginador = new Paginator(
+            array(
+                "data"  =>  $productos,
+                "limit" =>  10,
+                "page"  =>  $numeroDePagina
+            )
+        );
+        $this->view->page = $paginador->getPaginate();
     }
     /**
      * Muestra la vista de crear nuevos productos
